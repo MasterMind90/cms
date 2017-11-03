1732367fb9dce8ada8cddf4b404f45df83a6c676
Muhammad Amirul Ashraf <asdacap@gmail.com>
2017-11-03 12:43:47 +0800

    Fix buildContest not working when passing different contest directory.

--- dockerscripts/buildContest.sh
+++ dockerscripts/buildContest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONTEST_DIR=$(realpath $DIR/../contest)
+CONTEST_DIR=$(realpath $1)
 
 docker run --rm -v $CONTEST_DIR:/contest/ asdacap/polygon-build
